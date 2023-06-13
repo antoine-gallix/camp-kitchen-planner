63f1f1b18e0d0a7a4958d28564d0a4a42c1fcfe6
Antoine Gallix <antoine.gallix@a2ei.org>
2023-06-13 22:00:23 +0200

    dialog util

---
+++ dialog.py
@@ -0,0 +1,18 @@
+def choose(choices, display=str):
+    print("choose an item:")
+    for index, choice in enumerate(choices):
+        print("\t", index, display(choice))
+    print()
+    chosen_item = None
+    while chosen_item is None:
+        answer = input("? ")
+        try:
+            chosen_index = int(answer)
+        except ValueError:
+            print("answer must be an integer")
+            continue
+        try:
+            chosen_item = choices[chosen_index]
+        except IndexError:
+            print("index out of range")
+    return chosen_item
