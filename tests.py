d751e99e68a086f630fa2d3caa691eddcd381241
Antoine Gallix <antoine.gallix@a2ei.org>
2023-06-19 17:04:51 +0200

    more parser tests

--- tests.py
+++ tests.py
@@ -25,7 +25,19 @@ def test__parse_item_line__unit():
     )
 
 
+def test__parse_item_line__spaces_in_ingredient():
+    assert loader.parse_item_line("12g sativa ganja") == loader.ItemData(
+        name="sativa ganja", number=12, unit="g"
+    )
+
+
 def test__parse_item_line__unit_and_spaces():
     assert loader.parse_item_line("  12  g    ganja  ") == loader.ItemData(
+        name="ganja", number=12, unit="g"
+    )
+
+
+def test__parse_item_line__parenthesis():
+    assert loader.parse_item_line("12g ganja (well dried)") == loader.ItemData(
         name="ganja", number=12, unit="g"
     )
