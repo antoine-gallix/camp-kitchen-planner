d3696c2fcdf51fca9f05e16f4b7114b5530ed7cb
Antoine Gallix <antoine.gallix@a2ei.org>
2023-06-23 00:11:18 +0200

    cleanup

--- planner/loader.py
+++ planner/loader.py
@@ -7,30 +7,20 @@ import yaml
 from planner import logger, models
 
 
-def load_yaml_file(file_path):
+def parse_recipe_file(file_path):
     file_content = Path(file_path).read_text()
     parts = map(lambda st: st.strip(), file_content.split("\n---"))
-    data = map(lambda part: yaml.load(part, yaml.Loader), list(parts)[:2])
-    return list(data)
-
-
-@dataclass
-class RecipeFileData:
-    header: dict
-    items: list
-    instructions: str
-
-
-def parse_recipe_file(recipe_file_path):
-    header, items, instructions = load_yaml_file(recipe_file_path)
-    return RecipeFileData(header=header, items=items, instructions=instructions)
+    header, items = list(
+        map(lambda part: yaml.load(part, yaml.Loader), list(parts)[:2])
+    )
+    return header, items
 
 
 def load_recipe_file(path):
     print(f"reading recipe from {path!r}")
-    file_data = parse_recipe_file(path)
-    recipe = models.Recipe.create(**file_data.header)
-    for line in file_data.items:
+    header, items = parse_recipe_file(path)
+    recipe = models.Recipe.create(**header)
+    for line in items:
         try:
             item = models.Item.create_item_from_line(line, recipe)
         except:
