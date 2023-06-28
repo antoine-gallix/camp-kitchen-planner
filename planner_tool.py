61a52d39e6440cb95847321a9dc497fe1cd03d3f
Antoine Gallix <antoine.gallix@a2ei.org>
2023-06-28 17:56:39 +0200

    cli tool

---
+++ planner_tool.py
@@ -0,0 +1,15 @@
+import click
+
+import planner.loader
+import planner.models
+
+
+@click.command()
+@click.argument("project")
+def plan(project):
+    planner.models.create_tables()
+    planner.loader.load_recipe_dir("recipes")
+    project = planner.loader.load_project_file(project)
+    project.print_summary()
+    print("--- shopping list ---")
+    project.print_shopping_list()
