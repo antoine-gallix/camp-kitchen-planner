96e21a342a6038179b44f14a9ecec3120e8c806f
Antoine Gallix <antoine.gallix@a2ei.org>
2023-06-28 21:56:35 +0200

    csv option in cli

--- planner_tool.py
+++ planner_tool.py
@@ -6,11 +6,15 @@ import planner.models
 
 @click.command()
 @click.argument("project")
-def plan(project):
+@click.option("--csv", is_flag=True)
+def plan(project, csv):
     planner.models.create_tables()
     planner.loader.load_ingredients_from_file("ingredients.yaml")
     planner.loader.load_recipe_dir("recipes")
     project = planner.loader.load_project_file(project)
-    project.print_summary()
-    print("--- shopping list ---")
-    project.print_shopping_list()
+    if csv is True:
+        project.print_csv_shopping_list()
+    else:
+        project.print_summary()
+        print("--- shopping list ---")
+        project.print_shopping_list()
