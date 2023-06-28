eaf3130538b26e0acc1a3ba5283fd83a097a5d52
Antoine Gallix <antoine.gallix@a2ei.org>
2023-06-28 17:56:52 +0200

    count recipes

--- planner/explore.py
+++ planner/explore.py
@@ -1,3 +1,5 @@
+from peewee import SQL, fn
+
 import planner
 
 
@@ -6,3 +8,8 @@ def view_recipes():
     for recipe in planner.models.Recipe.select():
         print()
         print(f"# {recipe.description}")
+
+
+def count_recipes():
+    count = planner.models.Recipe.select(fn.COUNT(SQL("*"))).scalar()
+    print(f"There are {count} recipes in the database")
