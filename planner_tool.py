4a26eb3a14edfcc2e4a1184464ddd296222048fc
Antoine Gallix <antoine.gallix@a2ei.org>
2023-06-30 05:10:00 +0200

    rescale as a command

--- planner_tool.py
+++ planner_tool.py
@@ -3,11 +3,13 @@ import click
 import planner.loader
 import planner.models
 
+main = click.Group()
 
-@click.command()
+
+@main.command()
 @click.argument("project")
 @click.option("--csv", is_flag=True)
-def plan(project, csv):
+def compute(project, csv):
     planner.models.create_tables()
     planner.loader.load_ingredients_from_file("ingredients.yaml")
     planner.loader.load_recipe_dir("recipes")
@@ -18,3 +20,13 @@ def plan(project, csv):
         project.print_summary()
         print("--- shopping list ---")
         project.print_shopping_list()
+
+
+@main.command()
+@click.argument("recipe")
+@click.argument("servings", type=click.FLOAT)
+def rescale(recipe, servings):
+    planner.models.create_tables()
+    recipe = planner.loader.load_recipe_file(recipe)
+    rescaled = recipe.rescale(servings)
+    print(rescaled.full())
