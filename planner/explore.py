8e3929dcf0949f1ccade1ecf0a8b47fad72eacba
Antoine Gallix <antoine.gallix@protonmail.com>
2024-04-12 20:30:44 +0200

    delete, rename

--- planner/explore.py
+++ planner/explore.py
@@ -2,7 +2,7 @@ from peewee import SQL, fn
 import planner
 
 
-def count_model(model) -> int:
+def count_instances(model) -> int:
     """Count instances of a model"""
     return model.select(fn.COUNT(SQL("*"))).scalar()
 
@@ -18,8 +18,3 @@ def print_instances(model):
             print(instance)
     else:
         print(f"no instances of {model.__name__} in database")
-
-
-def count_recipes():
-    """Print model instance count"""
-    print(f"There are {count_model(planner.models.Recipe)} recipes in the database")
