c807879db8e76588742cbe02c03d64c0879060ad
Antoine Gallix <antoine.gallix@protonmail.com>
2024-04-12 21:53:15 +0200

    config loader

--- planner/config.py
+++ planner/config.py
@@ -1,7 +1,7 @@
 import dynaconf
 
 config = dynaconf.Dynaconf(
-    settings_file="config.toml",
+    settings_file=["config-defaults.toml", "config.toml"],
     envvar_prefix="CKP_",
     load_dotenv=True,
     merge_enabled=True,
