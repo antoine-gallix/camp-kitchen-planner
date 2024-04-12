8f45cf81006e677116bdc073dee5970a65da5939
Antoine Gallix <antoine.gallix@protonmail.com>
2024-04-12 20:01:46 +0200

    generic model printer

--- planner/explore.py
+++ planner/explore.py
@@ -1,25 +1,25 @@
 from peewee import SQL, fn
-
 import planner
 
 
-def print_recipes():
-    print("-- Recipes --")
-    for recipe in planner.models.Recipe.select():
-        print()
-        print(f"# {recipe.description}")
-
-
-def count_model(model):
+def count_model(model) -> int:
+    """Count instances of a model"""
     return model.select(fn.COUNT(SQL("*"))).scalar()
 
 
+# ------------------------- display -------------------------
+
+
+def print_instances(model):
+    """Print a list of instances"""
+    if instances := model.select().get_or_none():
+        print(f"-- {model.__name__} --")
+        for instance in instances:
+            print(instance)
+    else:
+        print(f"no instances of {model.__name__} in database")
+
+
 def count_recipes():
+    """Print model instance count"""
     print(f"There are {count_model(planner.models.Recipe)} recipes in the database")
-
-
-def print_ingredients():
-    count = count_model(planner.models.Ingredient)
-    print(f"-- Ingredients ({count}) --")
-    for ingredient in planner.models.Ingredient.select():
-        print(f"{ingredient.name} ({ingredient.unit})")
