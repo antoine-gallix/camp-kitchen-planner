4aaf5f328d74d65491f7cf71acdf9aeb554fac7a
Antoine Gallix <antoine.gallix@protonmail.com>
2024-04-12 21:41:19 +0200

    reorg

--- planner/loader.py
+++ planner/loader.py
@@ -2,13 +2,28 @@ import re
 from dataclasses import dataclass
 from pathlib import Path
 
+
 import funcy
 import yaml
 from typing import Any
 from planner import logger, models
 
 
-def parse_recipe_file(
+# ------------------------- ingredients -------------------------
+
+
+def load_ingredients_from_file(file_path) -> None:
+    logger.info(f"loading ingredients from file {file_path}")
+    ingredient_data = list(yaml.load_all(Path(file_path).open(), Loader=yaml.Loader))
+    logger.info(f"file contains {len(ingredient_data)} ingredients")
+    for d in ingredient_data:
+        models.Ingredient.create(**d)
+
+
+# ------------------------- recipe -------------------------
+
+
+def _parse_recipe_file(
     file_path,
 ) -> tuple[str, Any, Any, Any | None]:
     """Parse sections from a recipe file
@@ -39,8 +54,9 @@ def parse_recipe_file(
 
 
 def load_recipe_file(path) -> models.Recipe:
+    """Reads a recipe file and load recipe in database"""
     logger.debug(f"loading recipe {str(path)!r}")
-    name, header, items, instructions = parse_recipe_file(path)
+    name, header, items, instructions = _parse_recipe_file(path)
     recipe = models.Recipe.create(
         name=name, serves=header["serves"], instructions=instructions
     )
@@ -54,9 +70,13 @@ def load_recipe_file(path) -> models.Recipe:
 
 
 def load_recipe_dir(path) -> None:
+    """Loads all recipe files in a directory"""
     logger.info(f"loading recipe directory: {str(path)!r}")
     for recipe_file in Path(path).iterdir():
         load_recipe_file(recipe_file)
+
+
+# ------------------------- project -------------------------
 
 
 def load_project_file(path) -> models.Project:
@@ -69,15 +89,10 @@ def load_project_file(path) -> models.Project:
     return project
 
 
+# ------------------------- IO -------------------------
+
+
 def dump_ingredients(file_path) -> None:
     logger.info(f"writing ingredients in {file_path!r}")
     serialized = [ingredient.dump() for ingredient in models.Ingredient.select()]
     yaml.dump_all(serialized, Path(file_path).open("w"))
-
-
-def load_ingredients_from_file(file_path) -> None:
-    logger.info(f"loading ingredients from file {file_path}")
-    ingredient_data = list(yaml.load_all(Path(file_path).open(), Loader=yaml.Loader))
-    logger.info(f"file contains {len(ingredient_data)} ingredients")
-    for d in ingredient_data:
-        models.Ingredient.create(**d)
