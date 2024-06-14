c13493143b764b0454a4f6bf7ad5253726ac6f9a
Antoine Gallix <antoine.gallix@protonmail.com>
2024-06-15 01:21:50 +0200

    clean

--- planner/database.py
+++ planner/database.py
@@ -5,7 +5,8 @@ from planner.config import config
 
 
 class DB:
-    _instance=None
+    _instance = None
+
     def __new__(cls):
         if not cls._instance:
             if config.get("in_memory", False):
@@ -14,13 +15,5 @@ class DB:
             else:
                 db_url = config.get("database_file", "database.db")
                 logger.debug(f"using database file: {db_url}")
-            cls._instance =peewee.SqliteDatabase(db_url)
+            cls._instance = peewee.SqliteDatabase(db_url)
         return cls._instance
-
-# def AAMV3_DataFiles() -> Bucket:
-#     global bucket
-#     try:
-#         return bucket
-#     except NameError:
-#         bucket = Bucket(AAMV3_BUCKET)
-#         return bucket
