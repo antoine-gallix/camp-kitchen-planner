b94c6a1acb9cf0d5da47591a50d75252ae076085
Antoine Gallix <antoine.gallix@protonmail.com>
2024-06-15 01:58:40 +0200

    config prefix

--- planner/config.py
+++ planner/config.py
@@ -2,7 +2,7 @@ import dynaconf
 
 config = dynaconf.Dynaconf(
     settings_file=["config-defaults.toml", "config.toml"],
-    envvar_prefix="CKP_",
+    envvar_prefix="PLANNER_",
     load_dotenv=True,
     merge_enabled=True,
 )
