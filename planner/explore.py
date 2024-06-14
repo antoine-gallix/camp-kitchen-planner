3df82b6416405592d2d89ec35df25f93d57c79fa
Antoine Gallix <antoine.gallix@protonmail.com>
2024-06-14 23:10:11 +0200

    refactor list

--- planner/explore.py
+++ planner/explore.py
@@ -1,28 +1,19 @@
-from peewee import SQL, fn
 from rich import print
 from rich.table import Table
 
 
-def count_instances(model) -> int:
-    """Count instances of a model"""
-    return model.select(fn.COUNT(SQL("*"))).scalar()
-
-
-# ------------------------- display -------------------------
-
-
 def print_instances(model):
     """Print a list of instances"""
-    if instances := model.select():
-        print(f"-- {model.__name__} [{len(instances)}] --")
-        for instance in instances:
-            print(f"- {instance}")
-    else:
+    instances = model.select()
+    if not instances:
         print(f"no {model.__name__} in the database")
+        return
+    for instance in instances:
+        print(f"- {instance}")
 
 
 def print_instances_table(model):
-    """Print a list of instances"""
+    """Print a table of instances"""
     instances = model.select()
     if not instances:
         print(f"no {model.__name__} in the database")
