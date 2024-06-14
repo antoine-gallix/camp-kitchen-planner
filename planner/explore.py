814fe8897aa606ec0cca78d66a0fd654d5d3b028
Antoine Gallix <antoine.gallix@protonmail.com>
2024-06-14 22:41:57 +0200

    generic model table

--- planner/explore.py
+++ planner/explore.py
@@ -1,7 +1,6 @@
-import peewee
 from peewee import SQL, fn
-
-import planner
+from rich import print
+from rich.table import Table
 
 
 def count_instances(model) -> int:
@@ -14,9 +13,27 @@ def count_instances(model) -> int:
 
 def print_instances(model):
     """Print a list of instances"""
-    if instances:= model.select():
+    if instances := model.select():
         print(f"-- {model.__name__} [{len(instances)}] --")
         for instance in instances:
             print(f"- {instance}")
     else:
         print(f"no {model.__name__} in the database")
+
+
+def print_instances_table(model):
+    """Print a list of instances"""
+    instances = model.select()
+    if not instances:
+        print(f"no {model.__name__} in the database")
+        return
+    table = Table(title=model.__name__)
+    try:
+        fields = getattr(model, "_list_fields")
+    except AttributeError:
+        fields = model._meta.sorted_field_names
+    for field in fields:
+        table.add_column(field)
+    for instance in instances:
+        table.add_row(*tuple(str(getattr(instance, field)) for field in fields))
+    print(table)
