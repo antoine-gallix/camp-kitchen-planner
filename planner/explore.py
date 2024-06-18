0018d21e9a16f3c97bf14d829b32e4f21154288a
Antoine Gallix <antoine.gallix@protonmail.com>
2024-06-18 02:53:40 +0200

    compound accessors for fields

--- planner/explore.py
+++ planner/explore.py
@@ -18,6 +18,24 @@ def print_instances(model):
         print(f"- {instance}")
 
 
+def access_field(instance, accessor):
+    match accessor:
+        case str(attribute):
+            return getattr(instance, attribute)
+        case tuple((attribute, function)):
+            return function(getattr(instance, attribute))
+
+
+def field_as_string(field):
+    if isinstance(field, list):
+        if field:
+            return str([str(item) for item in field])
+        else:
+            return ""
+    else:
+        return str(field)
+
+
 def print_instances_table(model):
     """Print a table of instances"""
     instances = model.select()
@@ -26,22 +44,20 @@ def print_instances_table(model):
         return
     table = Table(title=model.__name__)
     try:
-        fields = getattr(model, "_list_fields")
+        field_accessor_specs = getattr(model, "_list_fields")
     except AttributeError:
-        fields = model._meta.sorted_field_names
-    for field in fields:
-        table.add_column(field)
+        field_accessor_specs = model._meta.sorted_field_names
+    for field_accessor_spec in field_accessor_specs:
+        match field_accessor_spec:
+            case str(name):
+                table.add_column(name)
+            case tuple((name, function)):
+                table.add_column(f"{function.__name__}({name})")
     for instance in instances:
-        instance_fields = (getattr(instance, field) for field in fields)
+        instance_fields = (
+            access_field(instance, field_accessor_spec)
+            for field_accessor_spec in field_accessor_specs
+        )
 
-        def as_string(field):
-            if isinstance(field, list):
-                if field:
-                    return str([str(item) for item in field])
-                else:
-                    return ""
-            else:
-                return str(field)
-
-        table.add_row(*tuple(as_string(field) for field in instance_fields))
+        table.add_row(*tuple(field_as_string(field) for field in instance_fields))
     print(table)
