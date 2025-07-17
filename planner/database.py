d76c93e256a4503bd7a53579d75c2af7576db1c6
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-07-17 11:54:20 +0200

    doc and refactor

--- planner/database.py
+++ planner/database.py
@@ -5,15 +5,27 @@ from planner.config import config
 
 
 class DB:
+    """Singleton Database Object
+
+    # usage
+    db=DB()
+    
+    """
     _instance = None
+
+    @staticmethod
+    def connect():
+        if config.get("in_memory", False):
+            db_url = ":memory:"
+            logger.debug("using in-memory database")
+        else:
+            db_url = config.get("database_file", "database.db")
+            logger.debug(f"using database file: {db_url}")
+        return peewee.SqliteDatabase(db_url)
+        
 
     def __new__(cls):
         if not cls._instance:
-            if config.get("in_memory", False):
-                db_url = ":memory:"
-                logger.debug("using in-memory database")
-            else:
-                db_url = config.get("database_file", "database.db")
-                logger.debug(f"using database file: {db_url}")
-            cls._instance = peewee.SqliteDatabase(db_url)
+            logger.debug("connecting to database")
+            cls._instance = cls.connect()
         return cls._instance
