a47c47b56f5fcbfa21fded70d7bccb7b981173c7
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-07-19 04:59:43 +0200

    file import function

--- planner/io.py
+++ planner/io.py
@@ -1,4 +1,10 @@
 import yaml
-import funcy
+import pathlib
 
-load_yaml = funcy.partial(yaml.load, Loader=yaml.Loader)
+
+def load_yaml(stream):
+    return yaml.load(stream, Loader=yaml.Loader)
+
+
+def load_all_yaml_from_file(file_path):
+    return yaml.load_all(pathlib.Path(file_path).read_text(), Loader=yaml.Loader)
